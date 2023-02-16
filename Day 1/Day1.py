ca6a4bb1cda48a0ecd700956fee381671ca4a016
Abby Dolata <125524084+AbbyDolata@users.noreply.github.com>
2023-02-16 02:41:07 +0000

    added print functions and stringing variables to day1.py

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,1 +1,16 @@
-input("Hello!")
+print("Hello World!")
+#print function 
+input("what is your name?")
+#input function
+print(len(input("What is your name? ")))
+#len function
+print("Hello"+" "+"Abby")
+#stringing variable 
+a=input("a: ")
+b=input("b: ")
+c=a
+a=b
+b=c
+print("a: "+ a)
+print("b: "+ b)
+#variables 
