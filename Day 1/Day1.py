f2d0620f15dbf724722b960ec599f8a2982b73df
Abby Dolata <125524084+AbbyDolata@users.noreply.github.com>
2023-02-16 03:01:55 +0000

    added project from day 1

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -14,3 +14,8 @@ b=c
 print("a: "+ a)
 print("b: "+ b)
 #variables 
+print("Hello! Welcome to the Band Name Generator. Let's begin!")
+I1=input("What is the name of the city you grew up in?\n")
+I2=input("What is the name of your first pet?\n")
+print(I1+" "+I2)
+#project 1, Band name generator
